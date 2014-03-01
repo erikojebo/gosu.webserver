f8594bd96b7c9f94c15e86319261fd77fed53762
Erik Öjebo <erik.ojebo@gmail.com>
2014-03-01 19:33:14 +0100

    Added response object

---
+++ dotnet/Gosu.WebServer/Gosu.WebServer.Specs/HttpResponseSpecs.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using NUnit.Framework;
+
+namespace Gosu.WebServer.Specs
+{
+    [TestFixture]
+    public class HttpResponseSpecs
+    {
+        [Test]
+        public void First_line_is_http_version_followed_by_status_code_and_description()
+        {
+            var response = new HttpResponse(HttpStatus.OK);
+
+            var responseString = response.ToString();
+
+            Assert.IsTrue(responseString.StartsWith("HTTP/1.1 200 OK\r\n"));
+        }
+    }
+}
---
+++ dotnet/Gosu.WebServer/Gosu.WebServer/HttpResponse.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace Gosu.WebServer
+{
+    public class HttpResponse
+    {
+        public readonly HttpStatus HttpStatus;
+
+        public HttpResponse(HttpStatus httpStatus)
+        {
+            HttpStatus = httpStatus;
+        }
+
+        public override string ToString()
+        {
+            var response = new StringBuilder();
+
+            response.AppendFormat("HTTP/1.1 {0} {1}\r\n", HttpStatus.StatusCode, HttpStatus.Description);
+
+            return response.ToString();
+        }
+    }
+}
